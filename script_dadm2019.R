d214a4fa61ceb8347a00d52bf8bca389d6e46f3e
cprevite <claudio.previte@unil.ch>
2019-10-16 11:29:05 +0200

    Simple EDA

--- script_dadm2019.R
+++ script_dadm2019.R
@@ -16,9 +16,18 @@ bands <- read.csv(file = here('bands3.csv'), sep = ';')
 
 summary(bands)
 
-bands <- apply(bands,2,toupper) # uppercase for all the values
+bands <- as.data.frame(lapply(bands,function(x) 
+  if(is.factor(x)) factor(toupper(x)) 
+  else(x))) # uppercase for all the factor values
+
+cols = c( 21:39)  
+bands[,cols] = apply(bands[,cols], 2, function(x) as.numeric(as.character(x))) # change class to numeric for numeric column
 
 str(bands)
+
+sapply(bands, mode)
+sapply(bands, class)
+
 
 describe(bands)
 
