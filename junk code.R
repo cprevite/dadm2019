b6b70e3dfec7c8362faf1b54a4883cc826c9b22e
cprevite <claudio.previte@unil.ch>
2019-12-14 18:27:35 +0100

    Test nnet

--- junk code.R
+++ junk code.R
@@ -27,3 +27,34 @@ bands <-
 #retrieve colnames
 a = as.data.frame((colnames(bands)))
 a = cbind(a,c(1:40))
+
+
+
+```{r}
+missing.values <- bands %>%
+  gather(key = "key", value = "val") %>%
+  mutate(isna = is.na(val)) %>%
+  group_by(key) %>%
+  mutate(total = n()) %>%
+  group_by(key, total, isna) %>%
+  summarise(num.isna = n()) %>%
+  mutate(pct = num.isna / total * 100)
+
+
+levels <-
+  (missing.values  %>% filter(isna == T) %>% arrange(desc(pct)))$key
+
+percentage.plot <- missing.values %>%
+  ggplot() +
+  geom_bar(aes(x = reorder(key, desc(pct)), 
+               y = pct, fill=isna), 
+           stat = 'identity', alpha=0.8) +
+  scale_x_discrete(limits = levels) +
+  scale_fill_manual(name = "", 
+                    values = c('steelblue', 'tomato3'), labels = c("Present", "Missing")) +
+  coord_flip() +
+  labs(title = "Percentage of missing values", x =
+         'Variable', y = "% of missing values")
+
+percentage.plot
+```
